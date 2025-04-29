2f6d6d0756b420242cf3fab9b54444b2b1a2ec79
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-04-30 04:40:09 +0530

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,6 +5,22 @@ const userController = {
         const alluser = await User.find()
 
         return res.json({ Result: alluser })
+    },
+
+    active_deactive_user: async(req, res) =>{
+        try{
+            const userEmail = req.params.userEmail
+
+            const checkuser = await User.findOne({ email: email })
+
+            if(!checkuser){
+                return res.json({ Error: "The User Cannot find at this time..."})
+            }
+            
+        }
+        catch(err){
+            console.log(err)
+        }
     }
 };
 
