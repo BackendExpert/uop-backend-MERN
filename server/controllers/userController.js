f857ed2e7c58561abe55daae35714280e6396c04
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-05-05 21:11:59 +0530

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -36,6 +36,42 @@ const userController = {
         catch(err){
             console.log(err)
         }
+    },
+
+    // chage user role
+
+    changeUserRole: async(req, res) => {
+        try{
+            const email = req.params.email
+
+            const {
+                role
+            } = req.body
+
+            const checkuser = await User.findOne({ email: email })
+
+            if(!checkuser){
+                return res.json({ Error: "No User Found..."})
+            }
+
+            const updateuser = await User.findOneAndUpdate(
+                { email: email },
+                [
+                    { $set: { role: role } }
+                ],
+                {new: true}
+            )
+
+            if(updateuser){
+                return res.json({ Status: "Success", Message: "User Updated Success"})
+            }
+            else{
+                return res.json({ Error: "Internal Server Error While Updating User"})
+            }
+        }
+        catch(err){
+            console.log(err)
+        }
     }
 };
 
