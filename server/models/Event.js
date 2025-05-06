8ec748391eafbb9b2246e28a31d9b9fe9360e285
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-05-06 22:36:58 +0530

    Update Event.js

--- server/models/Event.js
+++ server/models/Event.js
@@ -6,13 +6,14 @@ const EventSchema = new mongoose.Schema({
     description: { type: String, required: true },
     link: { type: String, required: true },
     image: { type: String, required: true },
+    isActive: { type: Boolean, required: true, default: false },
     isAccepted: { type: Boolean, required: true, default: false },
     addby: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true
     }
-});
+}, { timestamps: true });
 
 const Event = mongoose.model('Event', EventSchema);
 
