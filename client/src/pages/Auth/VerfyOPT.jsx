f3ba768a0e39fcc0cc899b640bfcffc0c3e993f2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-05-06 07:15:11 +0530

    Update VerfyOPT.jsx

--- client/src/pages/Auth/VerfyOPT.jsx
+++ client/src/pages/Auth/VerfyOPT.jsx
@@ -1,19 +1,87 @@
-import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import secureLocalStorage from 'react-secure-storage'
+import axios from 'axios'
+import DefultButton from '../../components/Buttons/DefultButton'
+import DefultInput from '../../components/Form/DefultInput'
 
 const VerfyOPT = () => {
     const navigate = useNavigate()
     const email = secureLocalStorage.getItem('email')
+    const [errorMsg, setErrorMsg] = useState(null);
+    const [otpdata, setotpdata] = useState({
+        otp: ''
+    })
 
-    if (email === '' || email.length === 0) {
+    const handleChange = (e) => {
+        setotpdata({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const headleVerifyOTP = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.post(import.meta.env.VITE_APP_API + '/auth/verifyOPT/' + email, otpdata)
+                .then(res => {
+                    if (res.data.Status === "Success") {
+                        setErrorMsg({ type: "success", message: res.data.Message });
+                        setTimeout(() => {
+                            navigate('/');
+                        }, 2000);
+                    }
+                    else {
+                        setErrorMsg({ type: "error", message: res.data.Error });
+                    }
+                })
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }
+
+
+    if (email === '') {
         return (
-            <div>VerfyOPT</div>
+            <div className="max-w-md mx-auto mt-16 bg-white p-8 rounded-3xl shadow-2xl">
+                <h2 className="text-2xl font-bold text-center text-blue-600 mb-6">Register</h2>
+
+                {errorMsg && (
+                    <div
+                        className={`${errorMsg.type === 'error' ? 'text-red-600 bg-red-300/20' : 'text-green-600 bg-green-300/20'
+                            } p-4 rounded-xl mb-4 transition-all duration-300 ease-in-out transform`}
+                        style={{ transform: errorMsg ? 'translateX(0)' : 'translateX(100%)', transition: 'transform 0.5s ease-out' }}
+                    >
+                        <div className="flex justify-between">
+                            <div className="flex">
+                                <p className="font-bold">{errorMsg.type === 'error' ? 'Error :' : 'Success :'}</p>
+                                <p className="pl-2">{errorMsg.message}</p>
+                            </div>
+                            <div className="mt-0">
+                                <MdOutlineClose className='h-6 w-auto cursor-pointer' onClick={() => setErrorMsg(null)} />
+                            </div>
+                        </div>
+                    </div>
+                )}
+
+                <form onSubmit={headleVerifyOTP}>
+                    <DefultInput
+                        label="Enter OTP (OTP is send to your registation email address)"
+                        type="text"
+                        name="otp"
+                        value={otpdata.otp}
+                        onChange={handleChange}
+                        placeholder="Enter your OTP"
+                        required
+                    />
+                    <div className="text-center mt-6">
+                        <DefultButton text="Verify OTP" btntype="submit" />
+                    </div>
+                </form>
+            </div>
         )
     }
     else {
         useEffect(() => {
-            navigate('/', { replace: true })
+            navigate('/register', { replace: true })
         }, [])
     }
 
