a2e911ac2cb162d807afa142d31154e2bf05cdeb
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-05-06 06:49:44 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,6 +5,7 @@ const validator = require('validator')
 const jwt = require('jsonwebtoken')
 const crypto = require('crypto');
 const UserActivity = require('../models/UserActivity');
+const UserOTP = require('../models/UserOTP');
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -63,9 +64,52 @@ const authController = {
                 const reusltnewact = await newactvitiy.save()
 
                 if(reusltnewact){
-                    
+                    const generateRandomCode = (length = 10) => {
+                        const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}';
+                        let code = '';
+                        for (let i = 0; i < length; i++) {
+                            code += chars.charAt(Math.floor(Math.random() * chars.length));
+                        }
+                        return code;
+                    };
+                
+                    const verificationCode = generateRandomCode();
 
-                    return res.json({ Status: "Success", Message: "Regisataion Successful.., Please wait for your account to be activated by the administrator."})
+                    const mailOptions = {
+                        from: process.env.EMAIL_USER,
+                        to: email,
+                        subject: 'Pdn Account Verification Code',
+                        html: `
+                            <p>Dear ${username},</p>
+                            <p>Thank you for registering at the University of Peradeniya system.</p>
+                            <p>Your verification code is:</p>
+                            <h2 style="color:#7c340c;">${verificationCode}</h2>
+                            <p>Please wait until your account is activated by an administrator.</p>
+                            <br>
+                            <p style="color:gray;">Do not share this code with anyone.</p>
+                        `,
+                    };
+
+                    const hashotp = await bcrypt.hash(verificationCode, 10)
+
+                    const storeOTP = new UserOTP({
+                        email: email,
+                        otp: hashotp
+                    })
+
+                    const resultstore = await storeOTP.save()
+
+                    transporter.sendMail(mailOptions, (err, info) => {
+                        if (err) {
+                            return res.json({ Error: "Registration succeeded, but failed to send verification email." });
+                        } else {                           
+                            return res.json({
+                                Status: "Success",
+                                Message: "Registration successful. Verification code sent to your email. Verify Email Please wait and wait for activation."
+                            });
+                        }
+                    });
+
                 }
                 else{
                     return res.json({ Error: "Internal Server Error"})
